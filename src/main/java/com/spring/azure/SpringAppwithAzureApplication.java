416fce9b7738489c378bc940a26d2af02ac12d0e
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-05-08 12:31:21 -0300

    spring and azure

---
+++ src/main/java/com/spring/azure/SpringAppwithAzureApplication.java
@@ -0,0 +1,17 @@
+package com.spring.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class SpringAppwithAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppwithAzureApplication.class, args);
+	}
+
+	
+
+}
